c358ba3b8d10800575e13f9054551277da7be74c
m5n <m5n.github@gmail.com>
2008-10-11 12:00:00 -0800

    Historic commit for v0.2: allow sort by user rating and average rating, improve performance, enable Update Queue button, mark changed rows, scroll back to top when sort is done.

--- netflix-queue-sorter.user.js
+++ netflix-queue-sorter.user.js
@@ -3,7 +3,7 @@
 // This is a Greasemonkey user script.
 //
 // Netflix Queue Sorter
-// Version 0.1, 2008-10-07
+// Version 0.2, 2008-10-11
 // Coded by Maarten van Egmond.  See namespace URL below for contact info.
 // Released under the GPL license: http://www.gnu.org/copyleft/gpl.html
 //
@@ -11,8 +11,8 @@
 // @name        Netflix Queue Sorter
 // @namespace   http://tenhanna.com/greasemonkey
 // @author      Maarten
-// @version     0.1
-// @description v0.1: Sort your queue.  Preview the new order.  Save it.
+// @version     0.2
+// @description v0.2: Sort your queue by user or average rating.  Review the new ordering.  Save it.
 // @include     http://www.netflix.com/Queue*
 // ==/UserScript==
 //
@@ -22,9 +22,10 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 //
-// This script allows you to sort your DVD or Instant Queue.  
-// It is strongly recommended you inspect the new ordering before making it
-// permanent via the Update Queue button.
+// This script allows you to sort your DVD or Instant Queue by user rating or 
+// average rating.  It just updates the list order input boxes and stops there.
+// This allows you, the user, to review the changes before updating your queue.
+// Once you press the Update Queue button, there's no undo.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -47,31 +48,54 @@ var NetflixQueueSorter = (function() {
         for (var idx in elts) {
             // Only for the first two instances. Skip the Saved Queue.
             if (idx < 2) {
-                var buttonInfo = _createSortButton();
+                // Sort by user rating.
+                var buttonInfo = _createSortButton(
+                       'usrRating', "Sort by User Rating", _reorderQueue);
                 _sortButtons.push(buttonInfo);
                 elts[idx].appendChild(buttonInfo.button);
+
+                // Sort by average rating.
+                buttonInfo = _createSortButton(
+                       'avgRating', "Sort by Average Rating", _reorderQueue);
+                _sortButtons.push(buttonInfo);
+                elts[idx].appendChild(buttonInfo.button);
+
+                // Undo.
+                buttonInfo = _createSortButton(
+                       'undo', "Undo Sort", _reorderQueue);
+                _sortButtons.push(buttonInfo);
+                elts[idx].appendChild(buttonInfo.button);
+                // Undo button is disabled by default.
+                _setButtonState(buttonInfo.button, false);
             }
         }
     }
 
-    function _createSortButton() {
+    function _createSortButton(value, label, onClickFn) {
         var button = document.createElement('button');
         button.setAttribute('type', 'button');
+        button.setAttribute('value', value);
         button.setAttribute('style',
-                'margin-left: 1em; position: relative; top: -6px');
-        var buttonText = document.createTextNode("Sort By Rating");
+                'margin-left: 0.75em; position: relative; top: -6px');
+        var buttonText = document.createTextNode(label);
         button.appendChild(buttonText);
-        button.addEventListener('click', _reorderQueue, true);
+        button.addEventListener('click', onClickFn, true);
         return {
             'button': button,
             'text': buttonText
         };
     }
 
-    function _reorderQueue() {
+    function _reorderQueue(evt) {
         // Prevent the user from pressing the buttons again.
         for (var idx = 0; idx < _sortButtons.length; idx++) {
             _setButtonState(_sortButtons[idx].button, false);
+        }
+        // Disable Update Queue button (well, we're really faking it).
+        for (var idx = 1; idx <= 2; idx++) {
+            var elt = document.getElementById('updateQueue' + idx);
+            elt.src = "http://cdn-0.nflximg.com/us/buttons/queueUpdate/instant_queue_inactive.gif";
+            _setButtonState(elt, false);
         }
 
         // Let GUI redraw buttons.
@@ -79,24 +103,25 @@ var NetflixQueueSorter = (function() {
             if (_sorted) {
                 _undoSort();
             } else {
-                _sortByRating();
+                var sortByAvgRating = evt.target.value == "avgRating";
+                _sortBy(sortByAvgRating);
             }
             _sorted = !_sorted;
 
-            // Change the button text and enable the buttons again.
+            // Enable the buttons again.
             for (var idx = 0; idx < _sortButtons.length; idx++) {
-                _replaceButtonText(_sortButtons[idx]);
-                _setButtonState(_sortButtons[idx].button, true);
+                var enabled = false;
+                if (_sorted) {
+                    // Only undo buttons should be enabled.
+                    enabled = _sortButtons[idx].button.value == "undo";
+                } else {
+                    // Only sort buttons should be enabled.
+                    enabled = _sortButtons[idx].button.value != "undo";
+                }
+                _setButtonState(_sortButtons[idx].button, enabled);
             }
         }
         setTimeout(delayed, 0);
-    }
-
-    function _replaceButtonText(buttonInfo) {
-        var oldElt = buttonInfo.text;
-        buttonInfo.text = document.createTextNode(
-                _sorted ? "Undo Sort" : "Sort By Rating");
-        buttonInfo.button.replaceChild(buttonInfo.text, oldElt);
     }
 
     function _setButtonState(button, enabled) {
@@ -107,8 +132,12 @@ var NetflixQueueSorter = (function() {
         }
     }
 
-    function _sortByRating() {
+    function _sortBy(sortByAvgRating) {
         _sortInfo = [];
+
+        // Don't take the whole document.body.innerHTML as text.
+        // Luckily there's a div containing just the items we need.
+        var text = document.getElementById('dvd-queue').innerHTML;
 
         var pos = 1;
 
@@ -117,21 +146,22 @@ var NetflixQueueSorter = (function() {
         // wherever you are in the string until the end-of-line, and any
         // lines underneath it.  To continue matching on another line,
         // skip into the line first using ".*?".
-        var regex = /(OR\d+)(?:.*?\n)*?.*?width:(.*?)px.*?([eg]):\s(.*?)</g;
-        while (regex.test(document.body.innerHTML)) {
-            var name = RegExp.$1;
+        var regex = /OR(\d+)(?:.*?\n)*?.*?width:(.*?)px.*?([eg]):\s(.*?)</g;
+        while (regex.test(text)) {
+            var id = RegExp.$1;
             var usrRating = RegExp.$2 / 95 * 5;   // mask of 95px is 5 stars
             var ratingType = RegExp.$3;
             var avgRating = RegExp.$4;
             _sortInfo.push({
-                "name": name,
+                "id": id,
                 "usrRating": usrRating,
                 "avgRating": avgRating,
                 "origPos": pos++
             });
         }
 
-        _quicksort(_sortInfo, 0, _sortInfo.length, "usrRating");
+        var algorithm = sortByAvgRating ? "avgRating" : "usrRating";
+        _quicksort(_sortInfo, 0, _sortInfo.length, algorithm);
 
         for (pos = 0; pos < _sortInfo.length; pos++) {
             _sortInfo[pos]["sortPos"] = _sortInfo.length - pos;
@@ -151,8 +181,19 @@ var NetflixQueueSorter = (function() {
 
         for (var pos = 0; pos < _sortInfo.length; pos++) {
             // Note: sortValue is 1-based, elts index is 0-based, so sub 1
-            elts[_sortInfo[pos][sortValue] - 1].value = _sortInfo.length - pos;
+            var elt = elts[_sortInfo[pos][sortValue] - 1];
+
+            // Set new value.
+            elt.value = _sortInfo.length - pos;
+
+            // If value was different than before, mark row as changed.
+            elt.focus();
+            elt.blur();   // Don't keep focus.
         }
+
+        // Move the page back to the top.
+        var elt = document.getElementById('MainQueueForm');
+        scroll(elt.offsetLeft, elt.offsetTop - 10);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -179,10 +220,10 @@ var NetflixQueueSorter = (function() {
         var store = begin;
         var ix;
         for (ix = begin; ix < end - 1; ++ix) {
-	    if (array[ix][sortValue] <= piv) {
-	        _swap(array, store, ix);
-	        ++store;
-	    }
+            if (array[ix][sortValue] <= piv) {
+                _swap(array, store, ix);
+                ++store;
+            }
         }
         _swap(array, end - 1, store);
 
